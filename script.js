fa0953fffc3518fc16572b912d00e7f510e492cf
maya <s218532125@gmail.ort.org.il>
2024-10-30 08:41:32 +0200

    another commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+let b = document.getElementById("b");
+let d = document.getElementById("d");
+b.onclick = function () {
+    d.innerText = "the button was clicked";
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsic2NyaXB0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxJQUFJLENBQUMsR0FBRyxRQUFRLENBQUMsY0FBYyxDQUFDLEdBQUcsQ0FBRSxDQUFDO0FBQ3RDLElBQUksQ0FBQyxHQUFHLFFBQVEsQ0FBQyxjQUFjLENBQUMsR0FBRyxDQUFFLENBQUM7QUFFdEMsQ0FBQyxDQUFDLE9BQU8sR0FBRztJQUNSLENBQUMsQ0FBQyxTQUFTLEdBQUcsd0JBQXdCLENBQUE7QUFFMUMsQ0FBQyxDQUFBIn0=
---
+++ script.ts
@@ -0,0 +1,7 @@
+let b = document.getElementById("b")!;
+let d = document.getElementById("d")!;
+
+b.onclick = function(){
+    d.innerText = "the button was clicked"
+    
+}
